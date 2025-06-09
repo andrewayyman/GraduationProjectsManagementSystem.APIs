2d7b44b63746260b7f4a0c4a66c85875049f89c2
Andrew Ayman <andrewayman1000@gmail.com>
2025-06-09 19:53:49 +0300

    trying to fix

---
+++ Repository/Migrations/20250609164705_testesraaerror.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class testesraaerror : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileUrl",
+                table: "TaskSubmissions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileUrl",
+                table: "TaskSubmissions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
